8febb9950d262736f8672462fc9a969faaa89587
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-11 17:01:05 +0100

    ReactFact Project - Markup

--- static_pages/src/main.tsx
+++ static_pages/src/main.tsx
@@ -1,4 +1,20 @@
 import { createRoot } from "react-dom/client";
 
+function MainSection() {
+  return (
+    <>
+      <img src="/reactLogo.png" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+}
+
 const root = createRoot(document.getElementById("root")!);
-root.render(<p>Hi from Zowa :3</p>);
+root.render(<MainSection />);
