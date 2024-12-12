a187711a423cb550d840be120fb7579abd96320f
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-12 15:25:13 +0100

    ReactFact Project - Main Content Section

--- static_pages/src/components/MainSection.tsx
+++ static_pages/src/components/MainSection.tsx
@@ -1,3 +1,14 @@
 export default function MainSection() {
-  return <h1>MainSection component</h1>;
+  return (
+    <main className="main">
+      <h1 className="h1">Fun facts about React</h1>
+      <ul className="list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
