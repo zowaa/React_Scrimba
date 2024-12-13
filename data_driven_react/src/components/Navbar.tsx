b7c221415643582252118874aeefe0569e0b5d7b
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-13 15:18:02 +0100

    Travel Journal - Header

---
+++ data_driven_react/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <header className="flex-container">
+      <img alt="logo" src="/logo.png" className="logo" />
+      <span className="title">my travel journal.</span>
+    </header>
+  );
+}
