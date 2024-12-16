350b60e977630b47eb1a15e580e108b0e13efdd8
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-16 21:46:06 +0100

    Chef Claude - Form

--- react_state/src/App.tsx
+++ react_state/src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <>
       <Header />
+      <div id="xx">
+        <Form />
+      </div>
     </>
   );
 }
---
+++ react_state/src/components/Form.tsx
@@ -0,0 +1,8 @@
+export default function Form() {
+  return (
+    <form id="form">
+      <input type="text" name="ingredient" placeholder="e.g. oregano" />
+      <button>+ Add ingredient</button>
+    </form>
+  );
+}
