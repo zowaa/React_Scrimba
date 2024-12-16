5d1a400d772775b260334b01577c2a34802d3ee8
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-16 17:29:16 +0100

    Travel Journal - Map Entry Components

--- data_driven_react/src/App.tsx
+++ data_driven_react/src/App.tsx
@@ -1,22 +1,26 @@
 import Navbar from "./components/Navbar";
 import Entry from "./components/Entry";
+import data from "./data";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <div id="main">
-        <Entry
-          img={{
-            alt: "Mount fuji",
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-          }}
-          country="Japan"
-          title="Mount Fuji"
-          dates="12 Jan, 2021 - 24 Jan, 2021"
-          link="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-          text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        />
+        {data.map((info) => (
+          <Entry
+            key={info.id}
+            img={{
+              alt: info.img.alt,
+              src: info.img.src,
+            }}
+            country={info.country}
+            title={info.title}
+            dates={info.dates}
+            link={info.googleMapsLink}
+            text={info.text}
+          />
+        ))}
       </div>
     </>
   );
