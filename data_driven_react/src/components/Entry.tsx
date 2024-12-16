2a80d84bc462c342abda76cae4d26d7920b3aedf
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-16 15:39:52 +0100

    Travel Journal - Passing props

--- data_driven_react/src/components/Entry.tsx
+++ data_driven_react/src/components/Entry.tsx
@@ -1,25 +1,24 @@
-export default function Entry() {
+interface Prop {
+  img: { alt: string; src: string };
+  country: string;
+  title: string;
+  dates: string;
+  link: string;
+  text: string;
+}
+
+export default function Entry(props: Prop) {
   return (
-    <div id="xx">
-      <article>
-        <img
-          className="img"
-          alt="location image"
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-        />
-        <div className="infos">
-          <img alt="location logo" src="/location.png" />
-          <span>JAPAN</span>
-          <a href="#">View on Google Maps</a>
-          <h2 className="place">Mount Fuji</h2>
-          <h3 className="dates">12 Jan, 2021 - 24 Jan, 2021</h3>
-          <p className="text">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
-        </div>
-      </article>
-    </div>
+    <article>
+      <img className="img" alt={props.img.alt} src={props.img.src} />
+      <div className="infos">
+        <img alt="location logo" src="/location.png" />
+        <span>{props.country}</span>
+        <a href={props.link}>View on Google Maps</a>
+        <h2 className="place">{props.title}</h2>
+        <h3 className="dates">{props.dates}</h3>
+        <p className="text">{props.text}</p>
+      </div>
+    </article>
   );
 }
