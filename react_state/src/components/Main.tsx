3ee0d67b87cdf8039881a951ec63863183372717
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-17 22:39:46 +0100

    Chef Claude - conditional rendering challenge1

--- react_state/src/components/Main.tsx
+++ react_state/src/components/Main.tsx
@@ -18,11 +18,25 @@ export default function Form() {
         <input type="text" name="ingredient" placeholder="e.g. oregano" />
         <button>+ Add ingredient</button>
       </form>
-      <ul>
-        {ingredients.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+      {ingredients.length != 0 && (
+        <section className="ingredient_section">
+          <h1>Ingredients on hands:</h1>
+          <ul>
+            {ingredients.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <div className="get_recipe">
+            <div className="container">
+              <span className="p1">Ready for a recipe</span>
+              <span className="p2">
+                Generate a recipe from your list of ingredients.
+              </span>
+            </div>
+            <button>Get a recipe</button>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
