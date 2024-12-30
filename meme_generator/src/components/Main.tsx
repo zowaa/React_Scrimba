8b8cdb489c8dd7be5ff46356434017ef691c7784
zowa <elfilfatimaezzahra0@gmail.com>
2024-12-30 17:11:05 +0100

    Meme Generator - Main done - Starting Point

--- meme_generator/src/components/Main.tsx
+++ meme_generator/src/components/Main.tsx
@@ -1,7 +1,27 @@
 export default function Main() {
   return (
     <main>
-      <p>hi zowa</p>
+      <div id="text">
+        <label>
+          Top text <br />
+          <input type="text" placeholder="Top text" />
+        </label>
+        <br />
+        <label>
+          Bottom text <br />
+          <input type="text" placeholder="Bottom text" />
+        </label>
+      </div>
+      <button>Get a new meme image 🖼</button>
+      <div
+        id="img"
+        style={{
+          backgroundImage: 'url("/img.png")',
+        }}
+      >
+        <p>SHUT UP</p>
+        <p>AND TAKE MY MONEY</p>
+      </div>
     </main>
   );
 }
